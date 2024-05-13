6ce326e65dc753c6f3cbcd9b6ec070ec0c0aa54e
conradsheeran <conradsheeran2005@gmail.com>
2024-05-13 21:04:48 +0800

    Cover remote repo

--- hello_world/hello_world.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()  // 此处 int 是对该函数返回值的定义
-{
-	printf("Hello, World!");
-	return 0;
-}
--- operation/operation.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	int a = 15;
-	float b = 18.5;
-	printf("%f", a * b);  // a*b 的值为一个浮点数，故为 %f
-	return 0;
-}
