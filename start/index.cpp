0e7fe1c89017e52c624106101204a6df00fa9269
conradsheeran <conradsheeran2005@gmail.com>
2024-05-13 23:55:24 +0800

    [Update]Learn how to use two func

--- start/index.cpp
+++ start/index.cpp
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #define pi 3.1415926
+float square(void);
+
 int main(void) {
 	int r;
-	float area;
 
 	r = 10;
-	area = pi * r * r;
-	printf("%f", area);
+	printf("%d", square(r));
 	return 0;
 
+}
+
+float square(int r) {
+	float area;
+
+	area = pi * r * r;
+	return area;
 }
