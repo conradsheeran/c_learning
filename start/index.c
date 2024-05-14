02c28e982e1bc5a3d6e067736535215944c797cc
conradsheeran <conradsheeran2005@gmail.com>
2024-05-14 22:38:39 +0800

    [Fix]Change the C++ file to the C file

--- start/index.c
+++ start/index.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #define pi 3.1415926
-float square(void);
+float square(int);
 
 int main(void) {
 	int r;
 
 	r = 10;
-	printf("%d", square(r));
+	printf("%f", square(r));
+
 	return 0;
 
 }
 
-float square(int r) {
+static float square(int r) {
 	float area;
 
 	area = pi * r * r;
+
 	return area;
 }
